8c175f52bafaded783023cc4833d94516cccc496
Gabriel Hounsome <imbeebo@gmail.com>
2014-09-11 20:58:57 -0400

    Added Navigation Functionality

--- myPortfolio/Scripts/script.js
+++ myPortfolio/Scripts/script.js
@@ -1,1 +1,12 @@
-﻿/// <reference path="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" />
+﻿/// <reference path="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" />
+
+
+$("#open_menu").click(function () {
+    $("#toggleBtn").css("visibility", "hidden");
+});
+
+$("#close_menu").click(function () {
+    $("#toggleBtn").css("visibility", "visible");
+});
+
+//.css({backgroundColor: "#ffe", borderLeft: "5px solid #ccc" })
