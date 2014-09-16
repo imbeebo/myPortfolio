edb341e75f7df8983c3e60d6ccee5a8dd24ea669
Gabriel Hounsome <imbeebo@gmail.com>
2014-09-16 14:46:36 -0400

    added about and contact page

---
+++ myPortfolio/email.php
@@ -0,0 +1,21 @@
+﻿<?php
+$email = $_POST['email'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+$ok=true;
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	echo 'you did not enter a valid email address<br />';
+	$ok=false;
+}
+if(empty($name))
+{
+	echo 'you must provide a name<br />';
+	$ok=false;
+}
+if($ok)
+{
+    mail('200265074@georgianc.on.ca', 'Email from '.$name, $message, 'From: '.$email);
+}
+	header("Location: index.html");
+?>
